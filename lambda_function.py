4dc2ee5379f375f60a7e89a20d1c6b75598ffaf4
Kyunghoon Kim <koorukuroo@gmail.com>
2024-02-05 17:24:22 +0900

    Add proxy integration output form

--- lambda_function.py
+++ lambda_function.py
@@ -8,3 +8,12 @@ def lambda_handler(event, context):
     result['status'] = 200
     result['message'] = 'Hello world'
     return result
+
+    # if you choise Proxy Integration,
+    # return {
+    #     'statusCode': 200,
+    #     'body': json.dumps(result),
+    #     'headers': {
+    #       'Access-Control-Allow-Origin': '*',
+    #     }
+    # }
